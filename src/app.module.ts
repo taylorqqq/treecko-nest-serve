6db3f3bc1b37c54cafd9522e1844b5db33f3e925
Treecko <h17683820991@163.com>
2023-04-19 00:27:13 +0800

    使用 ConfigModule 依赖包

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 // 装饰器 @Module() 用于标识一个模块
 // 完整的模块定义应该包含 imports、controllers、providers、exports 四个属性
 @Module({
-  imports: [], // 导入模块
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 全局配置
+    }),
+  ], // 导入模块
   controllers: [AppController], // 注册控制器
   providers: [AppService], // 注册服务
   // exports: [], // 导出服务
