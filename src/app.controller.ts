c1d1f23bfde8c6525082777d006e890c26a77bd9
Treecko <h17683820991@163.com>
2023-04-19 17:55:04 +0800

    转换数值管道 ParseIntPipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common'
 import { AppService } from './app.service' // 1.从容器中获取服务 2.提供类型支持
 import { PrismaClient } from '@prisma/client'
+import { HdPipe } from './hd/hd.pipe'
 
 @Controller() // 装饰器 @Controller() 用于标识一个控制器
 export class AppController {
@@ -10,17 +11,16 @@ export class AppController {
   } // 依赖注入
 
   @Get(':id') // 装饰器 @Get() 用于标识一个路由
-  getHello(@Param('id') id: number): any {
+  // HdPipe 等价于 ParseIntPipe
+  getHello(@Param('id', HdPipe) id: number): any {
     // 路由处理函数
     // return this.appService.getHello()
 
     // console.log(typeof id) // string
     // return id
-
-    // id 是BigInt类型
     return this.prisma.treecko_user.findUnique({
       where: {
-        id: 1,
+        id,
       },
     })
   }
