2786f9d8c0f88afb3cd025a9db0113af0ff3a729
Treecko <h17683820991@163.com>
2023-04-20 00:01:25 +0800

    自定义验证管道Validate 重写 系统验证管道ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { HdPipe } from './hd/hd.pipe'
 import { ParseIntPipe, ValidationPipe } from '@nestjs/common'
+import { Validate } from './validate/validate'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) // 创建 Nest 应用实例
-  // app.useGlobalPipes(new HdPipe()) // 全局管道HdPipe 数据处理
-  app.useGlobalPipes(new ValidationPipe()) // 全局管道ValidationPipe 校验
+  // app.useGlobalPipes(new HdPipe()) // 自定义全局管道HdPipe 数据处理
+  // app.useGlobalPipes(new ValidationPipe()) // 系统全局管道ValidationPipe 校验
+  app.useGlobalPipes(new Validate()) // 自定义全局管道ValidationPipe 校验
   await app.listen(3000) // 监听端口
 }
 bootstrap()
