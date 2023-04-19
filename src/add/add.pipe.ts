44592d4bb59366d3cd974d4bb364bca96ce4bb1f
Treecko <h17683820991@163.com>
2023-04-19 23:30:33 +0800

    管道校验优化

--- src/add/add.pipe.ts
+++ src/add/add.pipe.ts
@@ -1,4 +1,11 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common'
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common'
 import { plainToInstance } from 'class-transformer'
 import { validateSync } from 'class-validator'
 
@@ -8,8 +15,16 @@ export class AddPipe implements PipeTransform {
     const object = plainToInstance(metadata.metatype, value) // 将 value 转换为 metadata.metatype 类的实例
     const errors = validateSync(object)
     if (errors.length) {
-      const firstError = Object.values(errors[0].constraints)[0] || '参数错误'
-      throw new BadRequestException(`${firstError}`)
+      // const firstError = Object.values(errors[0].constraints)[0] || '参数错误'
+      // throw new BadRequestException(firstError)
+
+      const errorMessages = errors.map((error) => {
+        return {
+          name: error.property,
+          message: Object.values(error.constraints).map((v) => v),
+        }
+      })
+      throw new HttpException(errorMessages, HttpStatus.BAD_REQUEST)
     }
     // if (!value.name) {
     //   throw new BadRequestException('姓名不能为空')
